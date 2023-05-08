af223f73dcf33f19284120bfa4a41afd0761aec9
Emilio Ramos <emilioramos2001@gmail.com>
2023-05-08 16:14:46 -0400

    error con classpath

---
+++ src/main/java/org/example/CallgraphGenerator.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import java.io.File;
+import java.util.*;
+import soot.*;
+import soot.jimple.*;
+import soot.jimple.toolkits.callgraph.CallGraph;
+import soot.jimple.toolkits.callgraph.Edge;
+import soot.options.Options;
+import soot.toolkits.graph.*;
+import soot.util.*;
+
+public class CallgraphGenerator {
+    CallgraphGenerator(){
+        String libPath = "libs/";
+        Scene.v().setSootClassPath(Scene.v().getSootClassPath() + File.pathSeparator + libPath);
+
+        // Ruta al archivo APK de tu aplicación Android
+        String apkPath = "../apk_validation/com.amaze.filemanager_29.apk";
+
+        // Configurar las opciones de Soot
+        Options.v().set_src_prec(Options.src_prec_apk);
+        Options.v().set_process_dir(Collections.singletonList(apkPath));
+        Options.v().set_android_jars("../android-platforms/"); // Ruta a las bibliotecas de Android
+
+        // Cargar las clases y sus dependencias
+        Scene.v().loadNecessaryClasses();
+
+        // Habilitar la fase de análisis de CHA (Class Hierarchy Analysis)
+        PhaseOptions.v().setPhaseOption("cg.cha", "on");
+
+        // Obtener el Call Graph
+        CallGraph callGraph = Scene.v().getCallGraph();
+
+        // Imprimir el Call Graph
+        Iterator<Edge> edgeIterator = callGraph.iterator();
+        while (edgeIterator.hasNext()) {
+            Edge edge = edgeIterator.next();
+            SootMethod src = (SootMethod) edge.getSrc();
+            SootMethod tgt = (SootMethod) edge.getTgt();
+
+            System.out.println(src + " -> " + tgt);
+        }
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,37 @@
 package org.example;
 
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Map;
+
+import soot.Body;
+import soot.BodyTransformer;
+import soot.Local;
+import soot.PackManager;
+import soot.PatchingChain;
+import soot.RefType;
+import soot.Scene;
+import soot.SootClass;
+import soot.SootMethod;
+import soot.Transform;
+import soot.Unit;
+import soot.jimple.AbstractStmtSwitch;
+import soot.jimple.InvokeExpr;
+import soot.jimple.InvokeStmt;
+import soot.jimple.Jimple;
+import soot.jimple.StringConstant;
+import soot.jimple.toolkits.callgraph.CallGraph;
+import soot.options.Options;
+
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello, world!");
+
+        CallgraphGenerator cg = new CallgraphGenerator();
+
+        System.out.println("Bye, world!");
     }
+
 }
